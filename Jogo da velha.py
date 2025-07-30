e33a0b229023f6ef11b2d1b349ffc95a3e05c7e9
Natan Mauricio Santos <natanmauriciosantos@hotmail.com>
2025-07-30 14:43:40 +0000

    MENU PRINCIPAL

--- Jogo da velha.py
+++ Jogo da velha.py
@@ -9,3 +9,14 @@ def zerando_tabuleiro():
     for linha in range(3):
         for coluna in range(3):
             tabuleiro[linha][coluna] = "-"
+            
+def menu():
+    #Exibe as opções do programa
+    print("-" * 40)
+    print(" " * 13,"JOGO DA VELHA")
+    print("-" * 40)
+    print()
+    print("Opções:")
+    print("  1 - Novo jogo")
+    print("  2 - Placar")
+    print("  3 - Sair")
